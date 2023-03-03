02288c50775ce9bacc8eb15b2db5a71e2235a42f
misaelguedes <124177593+misaelguedes@users.noreply.github.com>
2023-03-03 14:16:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
+    if (hora >= 0 && hora < 6) {
+        // BOA MADRUGADA!
+        img.src = 'madrugada.png'
+        document.body.style.background = '#020420'
+    } else if (hora >= 6 && hora < 12) {
+        // BOM DIA!
+        img.src = 'manha.png'
+        document.body.style.background = '#ECDDBA'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'tarde.png'
+        document.body.style.background = '#F69960'
+    } else {
+        // BOA NOITE!
+        img.src = 'noite.png'
+        document.body.style.background = '#4D3B61'
+    }
+}
